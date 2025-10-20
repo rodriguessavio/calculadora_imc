4be215f6734d8d617942508ec8fd517cf6f135e1
rodriguessavio <capar.2022113tads0049@aluno.ifpi.edu.br>
2025-10-20 17:44:19 -0300

    feat(form): Blocking unnecessary digits

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -13,45 +13,60 @@ const ImcCalc = () => {
         setWeight('');
     }
 
+    const validDigits = (text) => {
+        return text.replace(/[^0-9,]/g, '');
+    }
 
-  return (
-    <div id='calc-container'>
-        <h2>Calculadora de IMC</h2>
-        <form id="imc-form">
-            <div className="form-inputs">
-                <div className="form-control">
-                    <label htmlFor="height">Altura: </label>
-                    <input 
-                        type="text" 
-                        name='height' 
-                        id='height'
-                        placeholder='Exemplo 1,75'
-                        onChange={(e) => setHeight(e.target.value)}
-                        value={height}
-                    />
+    const handleHeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value);
+        setHeight(updatedValue);
+    }
 
+    const handleWeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value);
+        setWeight(updatedValue);
+    }
+
+
+
+    return (
+        <div id='calc-container'>
+            <h2>Calculadora de IMC</h2>
+            <form id="imc-form">
+                <div className="form-inputs">
+                    <div className="form-control">
+                        <label htmlFor="height">Altura: </label>
+                        <input 
+                            type="text" 
+                            name='height' 
+                            id='height'
+                            placeholder='Exemplo 1,75'
+                            onChange={(e) => handleHeightChange(e)}
+                            value={height}
+                        />
+
+                    </div>
+
+                    <div className="form-control">
+                        <label htmlFor="height">Peso: </label>
+                        <input 
+                            type="text" 
+                            name='weight' 
+                            id='weight'
+                            placeholder='Exemplo 70,5'
+                            onChange={(e) => handleWeightChange(e)}
+                            value={weight}
+                        />
+                        
+                    </div>
                 </div>
-
-                <div className="form-control">
-                    <label htmlFor="height">Peso: </label>
-                    <input 
-                        type="text" 
-                        name='weight' 
-                        id='weight'
-                        placeholder='Exemplo 70,5'
-                        onChange={(e) => setWeight(e.target.value)}
-                        value={weight}
-                    />
-                    
+                <div className="action-control">
+                    <Button id="calc-btn" text="Calcular" />
+                    <Button id="clear-btn" text="Limpar" action={clearForm}/>
                 </div>
-            </div>
-            <div className="action-control">
-                <Button id="calc-btn" text="Calcular" />
-                <Button id="clear-btn" text="Limpar" action={clearForm}/>
-            </div>
-        </form>
-    </div>
-)
+            </form>
+        </div>
+    )
 }
 
 export default ImcCalc
